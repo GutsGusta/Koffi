ceeae3d0491e844db0495a59b80a09d8430b9c04
GutsGusta <gustavonovaes623@gmail.com>
2026-03-31 16:58:58 -0300

    adição de outras coisas

--- data.php
+++ data.php
@@ -1,3 +1,59 @@
 <?php
 $nomeloja = "Koffi";
 
+$categorias = [
+    'salgado' => 'Salgado',
+    'doce' => 'Doce',
+    'bebida' => 'Bebida'
+];
+
+$produtos_base = [
+    [
+        'id'=> 1,
+        'nome'=> 'Café',
+        'preco'=> 4.00,
+        'categoria'=> 'bebida',
+        'descricao'=> 'A alma da Koffi em uma dose intensa, com crema aveludada e notas profundas que despertam os sentidos.',
+        'imagem'=> 'imagens/cafe-expresso.jpg'
+    ],
+    [
+        'id'=> 2,
+        'nome'=> 'Donut',
+        'preco'=> 4.00,
+        'categoria'=> 'doce',
+        'descricao'=> 'Uma massa brioche incrivelmente leve e aerada, com o equilíbrio perfeito entre doçura e maciez.',
+        'imagem'=> 'imagens/Donut.png'
+    ],
+    [
+        'id'=> 3,
+        'nome'=> 'Croissant',
+        'preco'=> 6.00,
+        'categoria'=> 'salgado',
+        'descricao'=> 'Camadas folhadas que desmancham na boca, com aquele aroma amanteigado irresistível.',
+        'imagem'=> 'imagens/Croissant.jpg'
+    ],
+    [
+        'id'=> 4,
+        'nome'=> 'Cappuccino',
+        'preco'=> 4.00,
+        'categoria'=> 'bebida',
+        'descricao'=> 'A medida exata entre o vigor do café e a cremosidade do leite, finalizado com um toque de perfeição.',
+        'imagem'=> 'imagens/cappuccino.jpg'
+    ],
+    [
+        'id'=> 5,
+        'nome'=> 'Misto Quente',
+        'preco'=> 7.00,
+        'categoria'=> 'salgado',
+        'descricao'=> 'O clássico elevado a outro nível, com pão artesanal selado na manteiga e queijo derretendo.',
+        'imagem'=> 'imagens/misto-quente.jpg'
+    ],
+        [
+        'id'=> 6,
+        'nome'=> 'Brownie',
+        'preco'=> 6.00,
+        'categoria'=> 'doce',
+        'descricao'=> 'Denso, úmido e com aquela casquinha crocante que só um preparo artesanal consegue atingir.',
+        'imagem'=> 'imagens/brownie.jfif'
+    ],
+];
--- produtos.php
+++ produtos.php
@@ -22,108 +22,32 @@ include 'data.php';
             <div class="linha-titulo"></div>
         </div>
 
+        <div class="categorias">
+            <a>Todos</a>
+            <?php
+                foreach($categorias as $kcat => $nome) {
+                    print '<a href="#cat-'. $kcat .'">'. $nome .'</a>';
+                };
+            ?>
+            
+        </div>
+
         <div class="produtos">
-            <article class="card-produto">
-                <img src="imagens/cafe-expresso.jpg">
-                <h4>Café expresso</h4>
-                <h5>R$4,00</h5>
-                <p>A alma da Koffi em uma dose intensa, com crema aveludada e notas profundas que despertam os sentidos.</p>
+            <?php
+                foreach ($produtos_base as $produto) {
+                    print '<article class="card-produto">
+                <img src="'. $produto['imagem'] .'">
+                <h4>'.$produto['nome'].'</h4>
+                <h5>'.$produto['preco'].'</h5>
+                <p>'.$produto['descricao'].'</p>
                 <a href="detalhes-cafe.php" class="botao-card">Comprar agora</a>
             </article>
+                    ';
+                };
+            ?>
 
-            <article class="card-produto">
-                <img src="imagens/Donut.png">
-                <h4>Donut</h4>
-                <h5>R$4,00</h5>
-                <p>Uma massa brioche incrivelmente leve e aerada, com o equilíbrio perfeito entre doçura e maciez.</p>
-                <a href="detalhes-donut.php" class="botao-card">Comprar agora</a>
-            </article>
-
-            <article class="card-produto">
-                <img src="imagens/donut-cafe.jpg">
-                <h4>Donut + Café expresso</h4>
-                <h5>R$7,00</h5>
-                <p>A harmonização definitiva onde o vigor do café encontra a delicadeza do doce para uma experiência completa.</p>
-                <a href="detalhes-cafe-donut.php" class="botao-card">Comprar agora</a>
-            </article>
-
-            <article class="card-produto">
-                <img src="imagens/cappuccino.jpg">
-                <h4>Cappuccino</h4>
-                <h5>R$4,00</h5>
-                <p>Onde o amargor sofisticado do café encontra o abraço doce do donut. Um encontro de sabores que transforma o seu dia.</p>
-                <a href="#" class="botao-card">Comprar agora</a>
-            </article>
-        </div>
-
-        <div class="produtos">
-            <article class="card-produto">
-                <img src="imagens/Croissant.jpg">
-                <h4>Croissant</h4>
-                <h5>R$6,00</h5>
-                <p>A medida exata entre o vigor do café e a cremosidade do leite, finalizado com um toque de perfeição.</p>
-                <a href="#" class="botao-card">Comprar agora</a>
-            </article>
-
-            <article class="card-produto">
-                <img src="imagens/Pao-de-queijo.jpg">
-                <h4>Pão de Queijo</h4>
-                <h5>R$5,00</h5>
-                <p>Crocante por fora e elástico por dentro, com o sabor autêntico do queijo curado em cada mordida.</p>
-                <a href="#" class="botao-card">Comprar agora</a>
-            </article>
-
-            <article class="card-produto">
-                <img src="imagens/misto-quente.jpg">
-                <h4>Misto Quente</h4>
-                <h5>R$7,00</h5>
-                <p>O clássico elevado a outro nível, com pão artesanal selado na manteiga e queijo derretendo.</p>
-                <a href="#" class="botao-card">Comprar agora</a>
-            </article>
-
-            <article class="card-produto">
-                <img src="imagens/mediterranean.jpeg">
-                <h4>The Mediterranean</h4>
-                <h5>R$7,00</h5>
-                <p>Uma combinação sofisticada de presunto cru e rúcula que equilibra frescor e intensidade.</p>
-                <a href="#" class="botao-card">Comprar agora</a>
-            </article>
-        </div>
-
-        <div class="produtos">
-            <article class="card-produto">
-                <img src="imagens/brownie.jfif">
-                <h4>Brownie</h4>
-                <h5>R$6,00</h5>
-                <p>Denso, úmido e com aquela casquinha crocante que só um preparo artesanal consegue atingir.</p>
-                <a href="#" class="botao-card">Comprar agora</a>
-            </article>
-
-            <article class="card-produto">
-                <img src="imagens/torta-maca.avif">
-                <h4>Torta de Maçã</h4>
-                <h5>R$5,00</h5>
-                <p>Maçãs caramelizadas e especiarias em uma massa dourada que traz o conforto de um abraço.</p>
-                <a href="#" class="botao-card">Comprar agora</a>
-            </article>
-
-            <article class="card-produto">
-                <img src="imagens/cheesecake.avif">
-                <h4>Cheesecake</h4>
-                <h5>R$8,00</h5>
-                <p>Cremosa, levemente ácida e finalizada com uma calda artesanal que fecha a experiência com chave de ouro.</p>
-                <a href="#" class="botao-card">Comprar agora</a>
-            </article>
-
-            <article class="card-produto">
-                <img src="imagens/petit-gateau.jpg">
-                <h4>Petit Gâteau</h4>
-                <h5>R$10,00</h5>
-                <p>Um vulcão de chocolate intenso com centro derretido, servido com sorvete para o contraste perfeito.</p>
-                <a href="#" class="botao-card">Comprar agora</a>
-            </article>
-        </div>
     </main>
+
     <footer>
         <div class="footer-completo">
             <div class="parte-footer">
